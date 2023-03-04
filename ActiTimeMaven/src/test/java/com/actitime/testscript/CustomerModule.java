400216010b737648b9b7565c979fe10de9d34010
dell <dell@sumo>
2023-03-04 11:46:48 +0530

    Commit

---
+++ ActiTimeMaven/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	
+	@Test
+	public static void CusterModule() {
+		Reporter.log("CreateCustomer",true);
+	}
+
+}
